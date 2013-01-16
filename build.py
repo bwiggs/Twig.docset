3d45a4ac4fffe0e1fce876b8b700c09e7279a3dc
Brian Wigginton <brianwigginton@gmail.com>
2013-01-16 09:56:28 -0700

    Updating build script to use new Dash types

--- build.py
+++ build.py
@@ -2,9 +2,7 @@
 
 from bs4 import BeautifulSoup
 import glob
-import os
 import sqlite3
-import fnmatch
 
 RESOURCES = "Contents/Resources/"
 DOCUMENTS = RESOURCES + "Documents/"
@@ -23,20 +21,68 @@ print "Creating searchIndex"
 conn.execute("create table if not exists searchIndex (id integer PRIMARY KEY AUTOINCREMENT, name string, type string, path string)")
 conn.commit()
 
-# tags
-files = glob.glob(PATH + "doc/**/*.html")
+# filters
+files = [f for f in glob.glob(PATH + "doc/filters/*.html") if "index.html" not in f]
 for filename in files:
     print "Parsing: " + filename
     soup = BeautifulSoup(open(filename).read())
     sections = soup.find_all("h1")[1]
     section = sections.get_text()[:-1]
 
-    sql = SQL_INSERT % ( section, "func", filename[len(DOCUMENTS):] + "#" + sections.a['href'].split('#')[1] )
+    sql = SQL_INSERT % ( "Filter::" + section, "Function", filename[len(DOCUMENTS):] + "#" + sections.a['href'].split('#')[1] )
+    conn.execute(sql)
+    conn.commit()
+
+# functions
+files = [f for f in glob.glob(PATH + "doc/functions/*.html") if "index.html" not in f]
+for filename in files:
+    print "Parsing: " + filename
+    soup = BeautifulSoup(open(filename).read())
+    sections = soup.find_all("h1")[1]
+    section = sections.get_text()[:-1]
+
+    sql = SQL_INSERT % ( "Function::" + section, "Function", filename[len(DOCUMENTS):] + "#" + sections.a['href'].split('#')[1] )
+    conn.execute(sql)
+    conn.commit()
+
+# tags
+files = [f for f in glob.glob(PATH + "doc/tags/*.html") if "index.html" not in f]
+for filename in files:
+    print "Parsing: " + filename
+    soup = BeautifulSoup(open(filename).read())
+    sections = soup.find_all("h1")[1]
+    section = sections.get_text()[:-1]
+
+    sql = SQL_INSERT % ( section, "Tag", filename[len(DOCUMENTS):] + "#" + sections.a['href'].split('#')[1] )
+    conn.execute(sql)
+    conn.commit()
+
+# extensions
+files = [f for f in glob.glob(PATH + "doc/extensions/*.html") if "index.html" not in f]
+for filename in files:
+    print "Parsing: " + filename
+    soup = BeautifulSoup(open(filename).read())
+    sections = soup.find_all("h1")[1]
+    section = sections.get_text()[:-1]
+
+    sql = SQL_INSERT % ( section, "Module", filename[len(DOCUMENTS):] + "#" + sections.a['href'].split('#')[1] )
+    conn.execute(sql)
+    conn.commit()
+
+# tests
+files = [f for f in glob.glob(PATH + "doc/tests/*.html") if "index.html" not in f]
+for filename in files:
+    print "Parsing: " + filename
+    soup = BeautifulSoup(open(filename).read())
+    sections = soup.find_all("h1")[1]
+    section = sections.get_text()[:-1]
+
+    sql = SQL_INSERT % ( section, "Operator", filename[len(DOCUMENTS):] + "#" + sections.a['href'].split('#')[1] )
     conn.execute(sql)
     conn.commit()
 
 # GUIDES
-files = glob.glob(PATH + "doc/*.html")
+files = [f for f in glob.glob(PATH + "doc/*.html") if "index.html" not in f]
 for filename in files:
     print "Parsing: " + filename
     soup = BeautifulSoup(open(filename).read())
